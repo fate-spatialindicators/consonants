3a9582bf72b47729903cec0d231a583f0e1f7819
eric-ward <eric.ward@noaa.gov>
2019-08-09 09:26:38 -0700

    goa

---
+++ survey_data/create_ak_data.R
@@ -0,0 +1,23 @@
+library(dplyr)
+
+d = readRDS("survey_data/AK_BTS.rds")
+
+names(d) = tolower(names(d))
+
+# rename to match nwfsc
+d = dplyr::rename(d, species = common_name,
+  scientific_name = species_name,
+  latitude_dd = latitude,
+  longitude_dd = longitude,
+  cpue_kg_km2=cpue,
+  temp=gear_temperature,
+  depth=bottom_depth)
+
+# filter out goa
+d = dplyr::filter(d,survey=="GOA")
+
+# filter out 0 depths
+d = dplyr::filter(d,depth>0)
+
+saveRDS(d, "survey_data/joined_goa_data.rds")
+
