55054d9e0aa15904066cf19fd9ef80af13cb7c4f
eric-ward <eric.ward@noaa.gov>
2019-08-08 22:02:03 -0700

    re-configure script, convert to UTM, restrict range in lat/lon to observed data

--- code/run_wc_models.R
+++ code/run_wc_models.R
@@ -1,10 +1,25 @@
 library(sdmTMB)
 library(dplyr)
-
-# UTM transfromation
-# maybe subsetting hauls to reflect range of species
+library(sp)
 
 dat = readRDS("survey_data/joined_nwfsc_data.rds")
+
+dplyr::group_by(dat, species) %>% 
+  summarize(min = min(latitude[which(cpue_kg_km2 > 0)]),
+    max = max(latitude[which(cpue_kg_km2 > 0)])) %>% 
+  as.data.frame() %>% arrange(min)
+
+# UTM transformation
+dat_ll = dat
+coordinates(dat_ll) <- c("longitude_dd", "latitude_dd")
+proj4string(dat_ll) <- CRS("+proj=longlat +datum=WGS84")
+# convert to utm with spTransform
+dat_utm = spTransform(dat_ll, 
+  CRS("+proj=utm +zone=10 +datum=WGS84 +units=km"))
+# convert back from sp object to data frame
+dat = as.data.frame(dat_utm)
+dat = dplyr::rename(dat, longitude = longitude_dd, 
+  latitude = latitude_dd)
 
 df = expand.grid("species" = unique(dat$species),
   spatial_only=c(FALSE), 
@@ -16,21 +31,35 @@ saveRDS(df, "output/wc/models.RDS")
 
 for(i in 1:nrow(df)) {
   
+  # filter by species, and select range within occurrences
   sub = dplyr::filter(dat, 
-    species == df$species[i])
+    species == df$species[i]) %>% 
+    dplyr::filter(latitude > min(latitude[which(cpue_kg_km2>0)]),
+      latitude <= max(latitude[which(cpue_kg_km2>0)]),
+      longitude > min(longitude[which(cpue_kg_km2>0)]),
+      longitude < max(longitude[which(cpue_kg_km2>0)]))
+  
+  # rescale variables
   sub$depth = scale(log(sub$depth))
   sub$o2 = scale(log(sub$o2))
   sub$temp = scale(sub$temp)
 
   # drop points with missing values
-  sub = dplyr::filter(sub, year%in%seq(2010,2015)) %>% 
-    dplyr::filter(!is.na(o2),!is.na(temp),!is.na(depth))
+  sub = dplyr::filter(sub, 
+    !is.na(o2),!is.na(temp),!is.na(depth))
+  # filter years based on covariate
+  if(df$covariate[i]=="o2") {
+    sub = dplyr::filter(sub, year%in%seq(2010,2015))
+  } else {
+    # temp observed for all years
+    sub = sub
+  }
   
   # rename variables to make code generic
   sub = dplyr::rename(sub, enviro = as.character(df$covariate[i]))
   
   # make spde
-  spde <- make_spde(x = sub$longitude_dd, y = sub$latitude_dd, 
+  spde <- make_spde(x = sub$longitude, y = sub$latitude, 
     n_knots = 150)
   
   formula = paste0("cpue_kg_km2 ~ -1")
