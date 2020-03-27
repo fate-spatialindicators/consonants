b6ab4fcac11dced622f05bc111063d4a026ea4e0
eric-ward <eric.ward@noaa.gov>
2020-03-27 07:13:50 -0700

    typos and script

--- Figure S1.R
+++ Figure S1.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 library(ggplot2)
-
+library(ggsidekick)
 df = read.csv("output/wc_output.csv")
 #saveRDS(df,file=paste0("output/",region,"_output.rds"))
 
 wc_df = dplyr::filter(df, covariate=="temp") %>%
   dplyr::select(species,range,depth_effect) %>% 
   pivot_wider(names_from = depth_effect, values_from=range)
-wc_df$region = "West coast"
+wc_df$region = "USA West Coast"
 
 df = read.csv("output/goa_output.csv")
 goa_df = dplyr::filter(df, covariate=="temp") %>%
@@ -18,11 +18,13 @@ goa_df$region = "Gulf of Alaska"
 df = rbind(wc_df, goa_df) %>% 
   dplyr::rename("Depth included"="TRUE",
     "Depth omitted"="FALSE",
-    "Species"="species")
+    "Species"="species") %>% 
+  dplyr::mutate(`Depth included` = log(`Depth included`),
+    `Depth omitted` = log(`Depth omitted`))
 
+pdf("Figure_S1_range_sensitivity.pdf")
 ggplot(df, aes(`Depth included`,`Depth omitted`)) + 
-  geom_point() + scale_x_continuous(trans="log") + 
-  scale_y_continuous(trans="log") + 
-  facet_wrap(~region)
-
+  geom_point(col="dark blue",size=2,alpha=0.6) + 
+  facet_wrap(~region) + theme_sleek()
+dev.off()
 
