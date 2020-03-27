238a7e0252dc5dc50467ccf0884f37e439af7620
eric-ward <eric.ward@noaa.gov>
2020-03-27 15:58:53 -0700

    remove spp that weren't meant to be included

--- code/Figure S1.R
+++ code/Figure S1.R
@@ -9,11 +9,22 @@ wc_df = dplyr::filter(df, covariate=="temp") %>%
   pivot_wider(names_from = depth_effect, values_from=range)
 wc_df$region = "USA West Coast"
 
+# filter out the odd sharks and inverts
+wc_df = dplyr::filter(wc_df, species %in% c("pacific spiny dogfish",
+  "dungeness crab","brown cat shark","grooved tanner crab",
+  "filetail cat shark") == FALSE)
+
 df = read.csv("output/goa_output.csv")
 goa_df = dplyr::filter(df, covariate=="temp") %>%
   dplyr::select(species,range,depth_effect) %>% 
   pivot_wider(names_from = depth_effect, values_from=range)
 goa_df$region = "Gulf of Alaska"
+
+goa_df = dplyr::filter(goa_df, species %in% c("octopus unid.",
+  "Alaskan pink shrimp","sidestripe shrimp","squid unid.",
+  "shrimp unid.","Pacific sleeper shark","spiny dogfish",
+  "","magistrate armhook squid","giant octopus",
+  "chinook salmon","chum salmon") == FALSE)
 
 df = rbind(wc_df, goa_df) %>% 
   dplyr::rename("Depth included"="TRUE",
@@ -25,6 +36,6 @@ df = rbind(wc_df, goa_df) %>%
 pdf("plots/Figure_S1_range_sensitivity.pdf",height=5,width=8)
 ggplot(df, aes(`Depth included`,`Depth omitted`)) + 
   geom_point(col="dark blue",size=2,alpha=0.6) + 
-  facet_wrap(~region) + theme_sleek()
+  facet_wrap(~region) + theme_sleek()# + geom_text(aes(label=Species),size=3)
 dev.off()
 
