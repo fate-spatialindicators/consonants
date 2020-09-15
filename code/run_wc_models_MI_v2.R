252932257eff9de323bc13409c1b8ac2d6258cb4
Tim Essington <essing@Tims-iMac.local>
2020-09-15 13:34:37 -0700

    add function file, simplify MI_v2 model, add markdown summary file

--- code/run_wc_models_MI_v2.R
+++ code/run_wc_models_MI_v2.R
@@ -1,83 +1,13 @@
 #devtools::install_github("pbs-assess/sdmTMB")
+rm(list = ls())
+source("code/mi_functions.R")
+
 library(sdmTMB)
 library(dplyr)
 library(sp)
 library(gsw)
 library(ggplot2)
-
-dat = readRDS("survey_data/joined_nwfsc_data.rds")
-# analyze sablefish for years and hauls with adequate oxygen and temperature data, within range of occurrence
-dat = filter(dat, species == "sablefish", year%in%seq(2010,2015), 
-             !is.na(temp), !is.na(o2), !is.na(sal),
-             latitude_dd > min(latitude_dd[which(cpue_kg_km2>0)]),
-             latitude_dd <= max(latitude_dd[which(cpue_kg_km2>0)]),
-             longitude_dd > min(longitude_dd[which(cpue_kg_km2>0)]),
-             longitude_dd < max(longitude_dd[which(cpue_kg_km2>0)]))
-
-# compute metabolic index (mi) --------------------------------------------
-# converted from Halle Berger matlab script
-
-#O2 from trawl data is in ml/l - may need to be converted to umol/kg
-gas_const = 8.31
-partial_molar_vol = 0.000032
-kelvin = 273.15
-boltz = 0.000086173324
-
-#calculate percent saturation for O2 - assumes  units of mL O2/L
-# Input:       S = Salinity (pss-78)
-#              T = Temp (deg C) ! use potential temp
-#depth is in meters
-#[umole/kg] = [ml/L]*44660/(sigmatheta(P=0,theta,S) + 1000)
-dat$SA = gsw_SA_from_SP(dat$sal,dat$depth,dat$longitude_dd,dat$latitude_dd) #absolute salinity for pot T calc
-dat$pt = gsw_pt_from_t(dat$SA,dat$temp,dat$depth) #potential temp at a particular depth
-dat$CT = gsw_CT_from_t(dat$SA,dat$temp,dat$depth) #conservative temp
-dat$sigma0 = gsw_sigma0(dat$SA,dat$CT)
-dat$o2_umolkg = dat$o2*44660/(dat$sigma0+1000)
-# calc o2 solubility, relies on o2 in umol/kg
-gsw_O2sol_SP_pt <- function(sal,pt) {
-  x = dat$sal
-  pt68 = dat$pt*1.00024
-  y = log((298.15 - pt68)/(273.15 + pt68))
-  
-  a0 =  5.80871
-  a1 =  3.20291
-  a2 =  4.17887
-  a3 =  5.10006
-  a4 = -9.86643e-2
-  a5 =  3.80369
-  b0 = -7.01577e-3
-  b1 = -7.70028e-3
-  b2 = -1.13864e-2
-  b3 = -9.51519e-3
-  c0 = -2.75915e-7
-  
-  O2sol = exp(a0 + y*(a1 + y*(a2 + y*(a3 + y*(a4 + a5*y)))) + x*(b0 + y*(b1 + y*(b2 + b3*y)) + c0*x))
-  return(O2sol)
-}
-
-dat$O2_Sat0 = gsw_O2sol_SP_pt(dat$sal,dat$pt)
-
-#= o2satv2a(sal,pt) #uses practical salinity and potential temp - solubity at p =1 atm
-dat$press = exp(dat$depth*10000*partial_molar_vol/gas_const/(dat$temp+kelvin))
-dat$O2_satdepth = dat$O2_Sat0*dat$press
-
-#solubility at p=0
-dat$sol0 = dat$O2_Sat0/0.209
-dat$sol_Dep = dat$sol0*dat$press
-dat$po2 = dat$o2_umolkg/dat$sol_Dep
-
-# species-specific parameters
-Ao = 1.16625e-13
-Eo = 0.8736 # from cod, 0.8736.  Make it one half or double
-B = 3000 # size in grams, roughly average (initial calculations used 10000g)
-n = -0.208 # borrowed from cod 
-
-dat$mi = B^n*Ao*dat$po2/exp(-1*Eo/(boltz*(dat$temp+kelvin)))
-
-# prepare data and models -------------------------------------------------
-
-dat <- select(dat, species, year, longitude_dd, latitude_dd, cpue_kg_km2,
-              o2, temp, depth, mi, po2)
+dat <- load_data()
 
 # rescale variables
 dat$depth = scale(log(dat$depth))
@@ -106,56 +36,23 @@ dat = as.data.frame(dat_utm)
 dat = dplyr::rename(dat, longitude = longitude_dd, 
                     latitude = latitude_dd)
 
-# create combination of covariates and threshold responses for different models
-m_df = data.frame(
-  spatial_only = rep(TRUE,13), 
-  depth_effect = rep(TRUE,13),
-  time_varying = rep(FALSE,13),
-  threshold_function = c(rep("NA",8),rep("linear", 5)),
-  covariate1 = c("temp","o2","po2","mi",rep("temp",4),rep("o2",2), rep("po2", 2),"mi"),
-  covariate2 = c(rep("none",4),"o2","o2","po2", "po2","none", "temp", "none", "temp","none"),
-  interaction = c(rep(FALSE,5),TRUE, FALSE, TRUE, rep(FALSE,5)),
-  tweedie_dens = rep(NA,13) # set up vector to store performance data if using cv
-)
 
 # run models for each combination of settings/covariates in df ------------
 
 use_cv = FALSE # specify whether to do cross validation or not
+use_AIC = TRUE # specify whether to use AIC
 spde <- make_spde(x = dat$longitude, y = dat$latitude, n_knots = 250) # choose # knots
 
+m_df <- get_models();
+AICmat <- dAIC <- tweedie_dens <- matrix(NA, nrow = length(m_df), ncol =1 ) # set up array for tweedie density
+rownames(AICmat) <- rownames(dAIC) <- rownames(tweedie_dens) <- m_df
+
+
 # fit models with typical approach
-for(i in 1:nrow(m_df)){
-  print(paste0("model # ", i, " of ", nrow(m_df)))
-  formula = paste0("cpue_kg_km2 ~ 0 + as.factor(year)")
-  # rename variables to make code generic
-  sub <- dplyr::rename(dat, enviro1 = as.character(m_df$covariate1[i]))
+for(i in 1:length(m_df)){
+  print(paste0("model # ", i, " of ", length(m_df)))
   
-  # format data and formula based on combination of arguments in model settings df
-  if(m_df$threshold_function[i] == "linear") {
-    formula = paste0(formula, " + ", "breakpt(enviro1)")
-  }
-  if(m_df$threshold_function[i] == "logistic") {
-    formula = paste0(formula, " + ", "logistic(enviro1)")
-  } 
-  if(m_df$threshold_function[i] == "quadratic") {
-    formula = paste0(formula, " + ", "I(enviro1^2)")
-  }
-  if(m_df$threshold_function[i] == "NA") {
-    formula = paste0(formula, " + ", "enviro1")
-  }
-  # add 2nd covariate if included
-  if(m_df$covariate2[i] != "none") {
-    sub <- dplyr::rename(sub, enviro2 = as.character(m_df$covariate2[i]))
-    if(m_df$interaction[i] == TRUE){
-      formula = paste0(formula, " + ", "enviro2", " + ", "enviro1", " * ", "enviro2")
-    } else {
-      formula = paste0(formula, " + ", "enviro2")
-    }
-  }
-  
-  if(m_df$depth_effect[i]==TRUE) {
-    formula = paste0(formula, " + depth + I(depth^2)")
-  }
+  formula = paste0("cpue_kg_km2 ~ 0 +", m_df[i])
   
   # fit model with or without cross-validation
   if(use_cv==TRUE) {
@@ -170,7 +67,7 @@ for(i in 1:nrow(m_df)){
       seed = 10,
       family = tweedie(link = "log"),
       anisotropy = TRUE,
-      spatial_only = m_df$spatial_only[i]
+      spatial_only = TRUE
     ), silent = TRUE)
     
     if(class(m)!="try-error") {
@@ -181,13 +78,13 @@ for(i in 1:nrow(m_df)){
   } else {
     m <- try(sdmTMB(
       formula = as.formula(formula),
-      data = sub,
+      data = dat,
       time = NULL,
       reml = TRUE,
       spde = spde,
       family = tweedie(link = "log"),
       anisotropy = TRUE,
-      spatial_only = m_df$spatial_only[i]
+      spatial_only = TRUE
     ), silent = TRUE)
     
     if(class(m)!="try-error") {
@@ -197,48 +94,15 @@ for(i in 1:nrow(m_df)){
   }
 }
 
-
-back.convert <- function(x, mean_orig, sd_orig) (x* sd_orig+mean_orig)
-# Get AIC table
-
-
-
-
-
-#saveRDS(m_df, "output/wc/models_MI.rds")
-
-#as.list(m$sd_report, "Estimate")$b_threshold
-#as.list(m$sd_report, "Std. Error")$b_threshold
-
-# a few plots
-# residuals
-
-AICmat <- matrix(NA, nrow = 13, ncol =2)
-for (i in 1:nrow(m_df)) {
+# Use this if using AIC
+if (use_AIC) {
+for (i in 1:length(m_df)) {
   filename <- paste0("output/wc/model_",i,"_MI.rds")
   m <- readRDS(filename)
   AICmat[i,1] <-AIC(m)
 }
 
-dAIC <- AICmat[,1] - min(AICmat[,1])
-dAIC <- matrix(as.numeric(sprintf(dAIC,fmt = '%.2f')), nrow = 13, ncol = 1)
-rownames(dAIC) <- c("space + depth + year + temp", 
-                    "space + depth + year + o2",
-                    "space + depth + year + p02",
-                    "space + depth + year + mi",
-                    "space + depth + year+temp + o2",
-                    "space + depth + year + temp:o2",
-                    "space + depth + year+temp + po2",
-                    "space + depth + year + temp:po2",
-                    "space + depth + year + o2(breakpoint)",
-                    "space + depth + year + o2(breakpoint) + temp",
-                    "space + depth + year + po2(breakpoint)",
-                    "space + depth + year + po2(breakpoint) + temp",
-                    "space + depth + year + mi(breakpoint)")
+dAIC[,1] <- AICmat[,1] - min(AICmat[,1])
+dAIC[,1] <-as.numeric(sprintf(dAIC,fmt = '%.2f'))
 dAIC
-
-dAIC = data.frame(model = rownames(dAIC), dAIC = dAIC)
-
-
-
-
+}
